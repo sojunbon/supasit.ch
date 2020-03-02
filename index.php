c0c8c67b879204cfd0fd48d966652ddb7602320a
sojunbon <57395101+sojunbon@users.noreply.github.com>
2020-03-02 23:09:57 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="th">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Money Day credit</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap-4.3.1.css" rel="stylesheet">
+    <style type="text/css">
+    body,td,th {
+    color: #060606;
+}
+    </style>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+            <a class="nav-link" href="#">หน้าหลัก <span class="sr-only">(current)</span></a></li>
+			<a class="nav-link" href="http://line.me/ti/p/~0990588052">แอดไลน์ <span class="sr-only">(current)</span></a></li>
+
+			<!--
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Location</a>
+                <div class="dropdown-divider"></div>
+              </div>
+            </li> -->
+		 
+			
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit"></button>
+          </form>
+        </div>
+      </div>
+    </nav>
+	
+	<div class="container mt-3">
+      <div class="row">
+        <div class="col-12">
+			<div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="images/moneydays.jpg" alt="First slide">
+           
+              </div>
+          </div>
+	  </div>
+     </div>
+    </div>
+			
+	
+<hr>	 
+<h2 class="text-center"><strong>Money Day credit</strong></h2>
+<h2 class="text-center"><strong>บริการสินเชื่อเงินด่วน</strong> สำหรับท่านเจ้าของธุรกิจทุกประเภท </h2>
+	
+  <div class="container">
+	<div class="row">
+		<div class="col" align = 'left'> 
+			  <a href="tel:0805821698" target="_blank">
+				  <img class="rounded-0" src="images/Phone.jpg"></a>
+              <h4 class="text-center"></h4>
+          </div>
+		<div class="col" align = 'right'> 
+			<a href="http://line.me/ti/p/~0990588052">
+			<img class="rounded-0" src="images/Line.jpg"></a>
+              <h4 class="text-center"></h4>
+      </div>
+      </div>
+  </div>
+
+<!--
+  <div class="container">
+      <div class="row">
+        <div class="col-4">
+          <div class="row">
+            <div class="col-2"><img class="rounded-circle" alt="Free Shipping" src="images/40X40.gif"></div>
+            <div class="col-lg-6 col-10 ml-1">
+              <h4>Free Shipping</h4>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="row">
+            <div class="col-2"><img class="rounded-circle" alt="Free Shipping" src="images/40X40.gif"></div>
+            <div class="col-lg-6 col-10 ml-1">
+              <h4>Free Returns</h4>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="row">
+            <div class="col-2"><img class="rounded-circle" alt="Free Shipping" src="images/40X40.gif"></div>
+            <div class="col-lg-6 col-10 ml-1">
+              <h4>Low Prices</h4>
+            </div>
+          </div>
+        </div>
+		
+      </div>
+    </div>
+	-->
+    <hr>
+    <h2 class="text-center"></h2>
+	<div class="container">
+	<div class="row">
+		<div class="col" align = 'center'> 
+			  <img class="rounded-0" alt="100x100" src="images/J1.jpg"></a>
+			  <ul>
+				  <h2 class="text-left"></h2>	 
+			    
+			      <p class="card-text"style="font-size:22px; font-weight:lighter">ร้านอาหาร โรงแรม ที่พัก โรงงานผลิต โรงพิมพ์             </p>
+		       
+				
+			      <p class="card-text"style="font-size:22px; font-weight:lighter">บริษัทรับเหมา-ต่อเติม ร้านค้าขายส่งขนาดเล็ก-ใหญ่           </p>  
+		       
+				
+			      <p class="card-text"style="font-size:22px; font-weight:lighter">บริษัทนำเข้า-ส่งออก โรงงานอุตสาหกรรมและอีกหลากหลายธุรกิจฯ </p>	  
+		        
+	      </ul>
+          </div>
+      </div>
+  	</div>
+
+	<div class="container">
+	<div class="row">
+		<div class="col" align = 'center'> 
+			    <img class="rounded-0" alt="100x100" src="images/J2.jpg"></a>
+				<img class="rounded-0" alt="100x100" src="images/J3.jpg"></a>
+				<h2 class="text-left"></h2>
+              		<p class="card-text" style="font-size:22px; font-weight:lighter">หากท่านกำลังประสบปัญหาหมุนเงินไม่ทันเราช่วยคุณได้ ต้องการเงินด่วน เงินฉุกเฉิน ระยะสั้น ระยะยาว
+					โทรมาปรึกษาเราได้ บริการดี จริงใจ ให้วงเงินสูง</p>
+				<img class="rounded-0" alt="100x100" src="images/J4.jpg"></a>
+				<img class="rounded-0" alt="100x100" src="images/J5.jpg"></a>
+				<h2 class="text-left"></h2>
+				<h2 class="text-center">สามารถยื่นกู้ในเขตพื้นที่</h2>
+				<p class="card-text"style="font-size:22px; font-weight:lighter">
+					กรุงเทพ สมุทรปราการ สมุทรสาคร เพชรบุรี ปทุมธานี อยุธยา นครนายก สุพรรณบุรี</p>
+				<p class="card-text"style="font-size:22px; font-weight:lighter">
+					อ่างทอง กาญจนบุรี ปราจีนบุรี ฉะเชิงเทรา สระแก้ว</p>
+				<p class="card-text"style="font-size:22px; font-weight:lighter">
+					ทั้งภาคกลาง ภาคตะวันออก และภาคอีสาน</p>
+				<p class="card-text"style="font-size:22px; font-weight:lighter">
+					ก็สามารถยื่นกู้ได้</p>
+				
+				<img class="rounded-0" alt="100x100" src="images/J6.jpg"></a>
+			    <h2 class="text-center">จุดเด่นทางสินเชื่อ</h2>
+				<img class="rounded-0" alt="100x100" src="images/J8.jpg"></a>
+				
+         </div>
+
+      </div>
+  	</div>
+
+
+	<div class="container">
+	<div class="row">
+		<div class="col" align = 'center'> 
+			    <img class="rounded-0" alt="100x100" src="images/J7.jpg">
+		</div>
+      </div>
+  	</div>
+
+	<div class="container">
+	<div class="row">
+  		<div class="col" align = 'center'> 
+			    <img class="rounded-0" alt="100x100" src="images/J9.jpg">
+      	</div>
+
+      </div>
+  	</div>
+
+	<h2 class="text-center">โทรมาตอนนี้รับอัตราดอกเบี้ยพิเศษทันที</h2>
+	<div class="container">
+	<div class="row">
+		
+		<div class="col" align = 'center'> 
+			  <a href="tel:0805821698" target="_blank">
+			  <img class="rounded-0" alt="100x100" src="images/Phone.jpg"></a>
+              <h4 class="text-center"></h4>
+          </div>
+		<div class="col" align = 'right'> <a href="http://line.me/ti/p/~0990588052"><img class="rounded-0" alt="100x100" src="images/Line.jpg"></a>
+              <h4 class="text-center"></h4>
+      </div>
+
+      </div>
+  	</div>
+
+    <hr>
+	
+	
+	
+	
+	
+	
+  
+    <hr>
+
+    <div class="container text-white bg-dark p-4">
+      <div class="row">
+        <div class="col-6 col-md-8 col-lg-7">
+          <div class="row text-center">
+            <div class="col-sm-6 col-md-4 col-lg-4 col-12">
+              <ul class="list-unstyled">
+                <li class="btn-link"> <a></a> </li>
+              </ul>
+            </div>
+			  <!--
+            <div class="col-sm-6 col-md-4 col-lg-4 col-12">
+              <ul class="list-unstyled">
+                <li class="btn-link"> <a>Link anchor</a> </li>
+              </ul>
+            </div>
+			 
+            <div class="col-sm-6 col-md-4 col-lg-4 col-12">
+              <ul class="list-unstyled">
+                <li class="btn-link"> <a>Link anchor</a> </li>
+              </ul>
+            </div> -->
+          </div>
+        </div>
+        <div class="col-md-4 col-lg-5 col-6">
+          <address>
+          <p class="card-text"style="font-size:10px; font-weight:lighter">
+            สินเชื่อเงินด่วน ตัวช่วยสำหรับเจ้าของธุรกิจทุกท่าน รู้ผลไว อนุมัติง่าย ใน 30 นาที ให้วงเงินสูง คลอบคลุมทุกธุรกิจ ได้เงินไว ลดต้นลดดอก ไม่บานปลาย ธุรกิจไหนก็ทำได้</p>
+            <abbr title="Phone">Phone:</abbr> 080-5821698
+          </address>
+          <address>
+           <!-- <a href="mailto:#">first.last@example.com</a> -->
+          </address>
+        </div>
+      </div>
+    </div>
+    <footer class="text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <p>Copyright © GlobalSoft. All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap-4.3.1.js"></script>
+  <script src="js/touch.js"></script>
+  </body>
+</html>
